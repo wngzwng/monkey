98a5f26504c07a80dc234513e301f8352b042cd9
wngzwng <wngzwng@outlook.com>
2026-08-17 17:33:04 +0800

    新增中缀表达式 AST 节点并补充解析测试

--- ast/ast.go
+++ ast/ast.go
@@ -183,5 +183,32 @@ func (pe *PrefixExpression) String() string {
 	return out.String()
 }
 
+// ========== 中缀表达式 ==========
+type InfixExpression struct {
+	Token		token.Token	// 前缀词法单元，如 +
+	Left		Expression
+	Operator	string
+	Right		Expression
+}
+
+func (ie *InfixExpression) expressionNode() {}
+
+func (ie *InfixExpression) TokenLiteral() string {
+	return ie.Token.Literal
+}
+
+func (ie *InfixExpression) String() string {
+	var out bytes.Buffer
+
+	out.WriteString("(")
+	out.WriteString(ie.Left.String())
+	out.WriteString(ie.Operator)
+	out.WriteString(ie.Right.String())
+	out.WriteString(")")
+
+	return out.String()
+}
+
+
 
 
